72a03f4ab94f70afd00e990c767702857e7c6149
Manan-200 <manandeopurkar@gmail.com>
2022-06-20 18:48:29 +0530

    added descending sort

--- Exercise/arrange_arr.c
+++ Exercise/arrange_arr.c
@@ -6,11 +6,12 @@ int main(){
     int len = sizeof(arr)/sizeof(arr[0]);
     int high = 1;
 
-    int ms = 0;
+    int counter = 0;
 
-    for (int p1 = 0; p1 < len && high != arr[-1]; p1++){
-        for (int p2 = p1 + 1; p2 < len && high != arr[-1]; p2++){
-            ms++;
+    //Ascending
+    for (int p1 = 0; p1 < len/2; p1++){
+        for (int p2 = p1 + 1; p2 < len; p2 += 2){
+            counter++;
             if (arr[p2] != arr[p1] && arr[p2] == high){
                 int var = arr[p1];
                 arr[p1] = arr[p2];
@@ -21,11 +22,23 @@ int main(){
         high++;
     }
 
-    for (int p1 = 0; p1 < len; p1++){
-        printf("%d ", arr[p1]);
+    //Descending
+    for (int p1 = len/2 - 1; p1 < len; p1++){
+        for (int p2 = p1 + 1; p2 < len; p2++){
+            counter++;
+            if (arr[p1] != arr[p2] && arr[p2] == high){
+                int var = arr[p1];
+                arr[p1] = arr[p2];
+                arr[p2] = var;
+                break;
+            }
+        }
+        high--;
     }
 
-    printf("\nms : %d", ms);
+    for (int p1 = 0; p1 < len; p1++){printf("%d ", arr[p1]);}
+
+    printf("\ncounter : %d", counter);
     
     return 0;
 }
