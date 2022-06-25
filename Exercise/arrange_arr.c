f6f116478776949a50c8188378b3425278401ab1
Manan-200 <manandeopurkar@gmail.com>
2022-06-25 07:16:49 +0530

    faster algorithm

--- Exercise/arrange_arr.c
+++ Exercise/arrange_arr.c
@@ -2,42 +2,35 @@
 
 int main(){
     
-    int arr[10] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+    int arr[10] = {1, 1, 2, 2, 3, 3, 5, 5, 7, 7};
     int len = sizeof(arr)/sizeof(arr[0]);
-    int high = 2;
-
+    int *p2 = &arr[2];
     int counter = 0;
 
-    //Ascending
-    for (int p1 = 1; p1 < len/2; p1++){
-        for (int p2 = p1 + 1; p2 < len; p2 += 2){
-            counter++;
-            if (arr[p2] != arr[p1] && arr[p2] == high){
-                int var = arr[p1];
-                arr[p1] = arr[p2];
-                arr[p2] = var;
-                break;
-            }
-        }
-        high++;
+    //Ascending order
+    for (int *p1 = &arr[1]; p1 < &arr[len/2]; p1++)
+    {
+        *p1 = *p2;
+        p2 += 2;
+        counter++;
+    }
+    
+    p2 = &arr[len/2 - 1];
+
+    //Descending order
+    for (int *p1 = &arr[len/2]; p1 < &arr[len]; p1++)
+    {
+        *p1 = *p2;
+        p2--;
+        
+        counter++;
     }
 
-    //Descending
-    for (int p1 = len/2 - 1; p1 < len; p1++){
-        for (int p2 = p1 + 1; p2 < len; p2++){
-            counter++;
-            if (arr[p1] != arr[p2] && arr[p2] == high){
-                int var = arr[p1];
-                arr[p1] = arr[p2];
-                arr[p2] = var;
-                break;
-            }
-        }
-        high--;
+    //Printing arr
+    for (int *p = &arr[0]; p < &arr[len]; p++)
+    {
+        printf("%d ", *p);
     }
-
-    for (int p1 = 0; p1 < len; p1++){printf("%d ", arr[p1]);}
-
     printf("\ncounter : %d", counter);
     
     return 0;
